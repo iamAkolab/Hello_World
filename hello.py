d110ebf5b4154ebf3af2e18fa7a1b0b38c053d47
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-01-07 23:38:28 +0100

    add comments

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
 msg = "Hello Friend"
 print(msg)
 
+#ask for name
 print("What is your name?")
 myName = input()
 print("It is good to meet you, " + myName)
 print("The length of your name is ")
 print(len(myName))
 
+#ask for  age
 print("What is your age?")
 myAge = input()
 print("You will be " + str(int(myAge) + 1) + " next year")
