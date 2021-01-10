c9061f7a0e41c868f16bc8d1796202ffdf14b957
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-01-11 00:53:44 +0100

    added list.py....

---
+++ list.py
@@ -0,0 +1,12 @@
+list("hello")
+
+list(range(4))
+
+list(range(0, 100, 2))
+
+supplies = ['staplers', 'biro', 'pencil', 'binders', 'ruler', 'marker']
+
+for i in range(len(supplies)):
+    print("Index " + str(i) + " of the supplies is " + supplies[i])
+
+cat = ["fat"]
