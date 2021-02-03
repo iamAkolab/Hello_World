fc3013e164f36dbedd5005f494acb2c040a76146
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-03 01:27:45 +0100

    string methods...

---
+++ string_methods.py
@@ -0,0 +1,31 @@
+#String method
+#strings are immutable objects
+spam = "hello world!"
+
+spam.count()
+spam.title()
+spam.lower()
+spam.upper()
+
+spam.startswith()
+spam.endswith()
+
+spam.strip()
+spam.rstrip()
+spam.lstrip()
+
+spam.center(10,"-")
+spam.ljust()
+spam.rjust()
+
+
+
+#String formatting
+
+name =  "opomiulero"
+address = "19 chevron drive"
+deserts = "tulips pie"
+
+"Hello %s, the party is at %s, kindly print %s when you are coming", (name, address, deserts)
+
+#PyperpyModule
