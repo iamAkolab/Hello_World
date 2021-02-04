a8b7c261a1ef1ef9e989bccd22a027b30f65867a
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-04 23:02:11 +0100

    running from cmd....

---
+++ cmdpromptcodes.py
@@ -0,0 +1,14 @@
+#this tells the computer the version of python to use
+
+#! python 3
+
+print("Hello World")
+
+#this tells the computer to print out to command promt
+### @py c\directory\file.py %*
+
+#this tells the computer not to print out to command promt
+### @pyw c\directory\file.py %*
+
+#this tells the computer to pause the command promt after running
+### @pause
