cfcc9bbf035679a38cf657ec65f5bd9fb3ed3dcc
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-07 12:16:48 +0100

    findall method....

--- regex.py
+++ regex.py
@@ -1,5 +1,8 @@
 import re
 
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+
 mo = phoneNumRegex.search('Call me 415-555-1101 tomorrow, or ')
 print(mo.group())
+
+print(phoneNumRegex.findall('Call me 415-555-1101 tomorrow, or text on 415-555-1234'))
