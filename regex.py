a8ca4039d5bf42ed29097c4f848cbe7ec1380f07
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-09 00:21:30 +0100

    pipe style....

--- regex.py
+++ regex.py
@@ -5,6 +5,7 @@ phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 mo = phoneNumRegex.search('Call me 415-555-1101 tomorrow, or ')
 print(mo.group())
 
+#FindAll
 print(phoneNumRegex.findall('Call me 415-555-1101 tomorrow, or text on 415-555-1234'))
 
 
@@ -16,10 +17,13 @@ print(zoe.group())
 print(zoe.group(1))
 print(zoe.group(2))
 
-#Escape parenthesis when we looking for parenthesis
+#Escape parenthesis when we looking for match literal parenthesis
 bracNumRegex = re.compile(r'\(\d\d\d\) \d\d\d-\d\d\d\d')
 tash = bracNumRegex.search('Call me (415) 555-1101 tomorrow')
-
 print(tash.group())
 
-#Pipe
+#Pipe style can match one of many possible groups
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+zik = batRegex.search('Batmobile lost a harpoon')
+print(zik.group())
+print(zik.group(1))
