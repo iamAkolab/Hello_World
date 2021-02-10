679128e0a5bb7da30f779d390ff93426f123deb9
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-11 00:59:42 +0100

    absolute and relate files...

--- file_handling.py
+++ file_handling.py
@@ -3,3 +3,41 @@ import os
 print(os.path.join('folder1','folder2','folder3'))
 print(os.sep)
 print(os.getcwd())
+
+print(os.chdir(r"c:\\"))
+print(os.getcwd())
+
+#if the current working directory is bacon #
+#C:\bacon\fizz\spam.txt exist
+#C:\eggs\spam.txt exist
+
+
+#Relative                 Absolute
+#..\                      C:\ 
+
+#..\                      C:\spam.txt 
+
+#.\                       C:\bacon
+#.\fizz                   C:\bacon\fizz
+#.\fizz\spam.txt         C:\bacon\fizz\spam.txt 
+
+#..\eggs                   C:\eggs
+#..\eggs\spam.txt          C:\eggs\spam.txt 
+
+
+#so it is only the reference to the current directory that
+# has only one dot before the \
+
+#absolute functions
+print(os.path.abspath("dictionary.py"))
+print(os.path.isabs("c:\dictionary.py"))
+
+#relate function rgive the relative paths between 2 dir
+#os.path.relpath('c:\\folder1\\folder2\\spam.png', 'c:\\folder1')
+#>> 'folder2\\spam.png'
+
+print(os.path.dirname('c:\dictionary.py'))
+print(os.path.basename('c:\dictionary.py'))
+
+print(os.path.exists('c:\\folder1\\folder2\\spam.png'))
+print(os.path.isfile('c:\\windows\\system32\\calc.exe'))
