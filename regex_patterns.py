098c620be75aa6d2c8d219662d18774c6c537f39
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-14 00:20:09 +0100

    range regex......

--- regex_patterns.py
+++ regex_patterns.py
@@ -8,9 +8,9 @@ wo = batRegex.search('The Adventures of Batwoman')
 print(wo.group)
 
 no = batRegex.search('The Adventures of Batwowowowowowoman')
-print(no == None)
+print(no == None) #if true was not found
 
-
+# check for zero or more matches
 #we can also use this for number without area code
 phoneNumRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
 
@@ -18,4 +18,27 @@ mo = phoneNumRegex.search('Call me 415-555-1101 tomorrow, or ')
 print(mo.group())
 
 no = phoneNumRegex.search('Call me 555-1101 tomorrow, or ')
-print(no.group())
+print(no.group())#if true was not found
+
+#matches zero or more 
+zo = batRegex.search('The Adventures of Batwowowowowowoman')
+print(zo == None) #if true was not found
+
+#matches 1 or more (must occur)
+batRegex1 = re.compile(r'Bat(wo)+man')
+mo1 = batRegex1.search('The Adventures of Batwowowowowoman')
+print(mo1.group())
+
+#escaping * +
+regex = re.compile(r'\+\*\?')
+regex.search('I learned anout +*? regex syntax')
+
+#3 Phone numbers
+phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d(,)?')
+phone = phoneRegex.search("my numbers are 415-555-1101, 415-555-1102, 415-555-1103")
+print(phone.group())
+#range (start, end)
+
+haRegex = re.compile(r'(Ha)(3,5')
+ha = haRegex.search('He said "Hahahaha"')
+print(ha.group)
