dc49396d94b47cc24a747e9040c8168f1406e7df
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-13 23:33:57 +0100

    Update regex_patterns.py
    phone number

--- regex_patterns.py
+++ regex_patterns.py
@@ -9,3 +9,13 @@ print(wo.group)
 
 no = batRegex.search('The Adventures of Batwowowowowowoman')
 print(no == None)
+
+
+#we can also use this for number without area code
+phoneNumRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
+
+mo = phoneNumRegex.search('Call me 415-555-1101 tomorrow, or ')
+print(mo.group())
+
+no = phoneNumRegex.search('Call me 555-1101 tomorrow, or ')
+print(no.group())
