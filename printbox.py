586ec44339a28333827333913e4f19deb036eec5
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-14 01:01:38 +0100

    Update printbox.py
    completed printbox function

--- printbox.py
+++ printbox.py
@@ -1,4 +1,5 @@
-    """[summary]
+
+"""[summary]
 
     this function print box
 
@@ -8,7 +9,22 @@
     *            *
     *            *
     **************
-    """
+"""
 
 def printbox(symbol,width,height):
-    if len(symbol)
+    if len(symbol) != 1:
+        raise Exception ('"Symbol" needs to be astring of length 1.')
+    if (width <2) or (height < 2):
+        raise Exception ('"Width" and "Height" must be greater than or equal to 2')
+
+    print(symbol * width)
+
+    for i in range(height - 2):
+        print(symbol + ' ' * (width - 2) + symbol)
+
+    print(symbol * width)
+    
+#printbox('*', 2, 3) found bug that return flat
+printbox('*', 3, 3)
+printbox('*', 8, 5)
+
