7615d1239f0454ce27158736221ce68c9049b0cb
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-15 23:28:00 +0100

    send to trash module

--- delete_file.py
+++ delete_file.py
@@ -13,7 +13,7 @@ import shutil
 #delete folder, everything is deleted be careful!!!
 shutil.rmtree("C:\\Users\\ai")
 
-#before deleting always print out the file name to be sure
+#before deleting do a dry run by always print out the file name to be sure
 
 os.chdir("C:\\Users\\john\\desktop")
 
@@ -21,3 +21,7 @@ for filename in os.listdir():
     if filename.endswith('.txt'):
         #os.unlink(filename)
         print(filename)
+
+#send to trash
+#import send2trash
+#send2trash("C:\\Users\\john\\heart.txt")
