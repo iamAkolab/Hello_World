889ad99fd97c896c074749f4794a36711c5393a4
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-16 00:32:18 +0100

    added triangle quest....

--- printbox.py
+++ printbox.py
@@ -38,3 +38,7 @@ except:
     errorFile.write(traceback.format_exc())
     errorFile.close()
     print('The traceback info was written error_log.txt')
+
+#Triangle Quest Print a numerical triangle of height n-1
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(i*10**i//9)
