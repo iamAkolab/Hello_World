0aa826cdded7587347478dedc28cabd0a78d7648
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-20 15:01:10 +0100

    added the method....

--- webscraping.py
+++ webscraping.py
@@ -9,3 +9,14 @@ elems = soup.select('#mediaTab_heading_0 > a > span > div:nth-child(2) > span.a-
 
 print(elems[0].text)
 print(elems[0].text.strip())
+
+
+def getAmazonPrice(ProductUrl):
+    res = requests.get(ProductUrl)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    return elems[0].text.strip()
+
+price = getAmazonPrice('https://www.amazon.com/Automate-Boring-Stuff-Python-Programming/dp/1593275994')
+print('The price is '+ price)
