1b326f67769f2bde476c5279a18d7a44c6f5b1b0
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-21 02:43:01 +0100

    pdf editing.....

---
+++ pdf_editor.py
@@ -0,0 +1,22 @@
+import PyPDF2
+import os
+
+os.chdir(r'C:\\Users\\john\\Downloads')
+
+pdf1File = open('meetingminutes1.pdf','rb')
+pdf2File = open('meetingminutes2.pdf','rb')
+
+reader1 = PyPDF2.PdfFileReader(pdf1File)
+reader2 = PyPDF2.PdfFileReader(pdf2File)
+
+writer = PyPDF2.PdfFileWriter()
+
+for pageNum in range(reader1.numPages):
+    page = reader1.getPage(pageNum)
+    writer.addPage(page)
+
+for pageNum in range(reader2.numPages):
+    page = reader1.getPage(pageNum)
+    writer.addPage(page)
+
+open('combinedminutes.pdf','wb')
