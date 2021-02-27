929ffb88174e3b4de4fb53ad46f3033841ffb764
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-02-28 00:00:24 +0100

    get the screen coordinate the esasy way

--- guidraw_code.py
+++ guidraw_code.py
@@ -22,3 +22,18 @@ while distance > 0:
 
     #move up
     pyautogui.dragRel(0, -distance, duration = 0.1)
+
+'''
+To get the screen coordinate you need.
+
+C:\WINDOWS\system32>py -3.9
+
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import pyautogui
+>>> pyautogui.displayMousePosition()
+
+Press Ctrl-C to quit.
+
+X: 311 Y:274 RGB: (0. 0. 0.)
+'''
