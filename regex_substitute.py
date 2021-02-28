f9f7cc16cf1f1d8519ee2e005843e7fcf4b414b7
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-03-01 00:11:20 +0100

    substitue methods for replacing string.....

--- regex_substitute.py
+++ regex_substitute.py
@@ -1,1 +1,19 @@
-ex
+import re
+
+namesRegex = re.compile(r'Agent \w+')
+print(namesRegex.findall('Agent Alice gave the secret documents to Agent Bob.'))
+
+print(namesRegex.sub('REDACTED','Agent Alice gave the secret documents to Agent Bob.'))
+
+
+#Lets just use their first nme
+namesRegex1 = re.compile(r'Agent (\w)\w*')
+print(namesRegex1.findall('Agent Alice gave the secret documents to Agent Bob.'))
+
+print(namesRegex1.sub(r'Agent \1*****','Agent Alice gave the secret documents to Agent Bob.'))
+
+
+#print(namesRegex.sub('Agent \1*****','Agent Alice gave the secret documents to Agent Bob.'))
+#printed this \
+#Agent ☺***** gave the secret documents to Agent ☺*****
+
