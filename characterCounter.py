081c733188fac18664d428553b8af7caf4aaec6d
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-09-26 02:06:09 -0500

    comments

--- characterCounter.py
+++ characterCounter.py
@@ -1,6 +1,8 @@
+# create a string variable
 message = " When her father unexpectedly dies, young Ella finds herself at the mercy of her cruel stepmother and her scheming stepsisters"
 count = {}
 
+# loop to iterate variable
 for character in message.lower():
     count.setdefault(character,0)
     count[character] = count[character] + 1
