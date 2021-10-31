26ff0521a0951bacf2c861675afe8eb07cec5327
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-10-31 02:03:36 -0500

    added comments

--- dictionaries.py
+++ dictionaries.py
@@ -1,12 +1,16 @@
+# create a dictiona
 myPc = {"size": 14, "color":"silver", "os":"windows10"}
 
+# get value by key
 myPc["os"]
+
+# use the get value in print
 print("My laptop has " + myPc["color"] + " color")
 
-#Order matter for list
+# Order matter for list
 print([1,2,3] == [3,2,1])
 
-#Order does not matter for dictionary
+# Order does not matter for dictionary
 myPc1 = {"size": 14, "color":"silver", "os":"windows10"}
 myPc2 = { "os":"windows10", "size": 14, "color":"silver"}
 
@@ -14,7 +18,7 @@ print(myPc1 == myPc2)
 
 print("color" in myPc1)
 
-#Dictionary functions
+# Dictionary functions
 print(list(myPc1.keys()))
 
 print(list(myPc1.values()))
@@ -30,12 +34,12 @@ for j in myPc1.values():
 for k,v in myPc1.items():
     print(k,v)
 
-#Get methods: to prevent KeyError when the key does not exist
+# Get methods: to prevent KeyError when the key does not exist
 myPc2.get("os","")
 
 print(myPc2.get("siz",15))
 
-#SetDefault: set default value if it does not exist 
+# SetDefault: set default value if it does not exist 
 myPc2.setdefault("maker","Dell")
 print(myPc2)
 myPc2.setdefault("maker","hp")
