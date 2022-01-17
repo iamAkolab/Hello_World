61575cec09b3a3221b9eaa1d2bc84de1743b3562
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2022-01-16 23:39:33 -0600

    commit...

--- traffic_sim.py
+++ traffic_sim.py
@@ -19,12 +19,16 @@ def switchLights(intersection):
     """
     
     for key in intersection.keys():
+        
         if intersection[key] == 'green':
             intersection[key] == 'yellow'
+            
         elif intersection[key] == 'yellow':
             intersection[key] == 'red'
+            
         elif intersection[key] == 'red':
             intersection[key] == 'green'
+
     assert 'red' in intersection.values(), 'Neither light is red!' + str(intersection)
 
 
