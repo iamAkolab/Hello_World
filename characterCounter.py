05fc359adeac94557872a5ea97608f736bacbed5
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2022-01-20 01:55:06 -0600

    added the old codes to the functions

--- characterCounter.py
+++ characterCounter.py
@@ -15,15 +15,21 @@ def message_counter(msg):
 # create a string variable
 message = " When her father unexpectedly dies, young Ella finds herself at the mercy of her cruel stepmother and her scheming stepsisters"
 
-
+# call the function
 message_counter(message)
 
 '''
+# dictionary to hold the counter
+count = {}
+ 
+# create a string variable
+message = " When her father unexpectedly dies, young Ella finds herself at the mercy of her cruel stepmother and her scheming stepsisters"   
 
+# this is the old codes, I had to pa
 for character in message.lower():
     count.setdefault(character,0)
     count[character] = count[character] + 1
 
-# print
+# print the count
 print(count)
 '''
