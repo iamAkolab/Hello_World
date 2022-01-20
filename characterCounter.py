6e7ffb0796cdcf1f0958f7a2978fa17f836b58cf
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2022-01-20 01:52:30 -0600

    created the function for message counter

--- characterCounter.py
+++ characterCounter.py
@@ -1,10 +1,29 @@
+# create a function
+def message_counter(msg):
+    
+    # dictionary to hold the counter
+    count = {}
+    
+    # loop to iterate variable
+    for character in msg.lower():
+        count.setdefault(character,0)
+        count[character] = count[character] + 1
+        
+    # print
+    print(count)
+
 # create a string variable
 message = " When her father unexpectedly dies, young Ella finds herself at the mercy of her cruel stepmother and her scheming stepsisters"
-count = {}
 
-# loop to iterate variable
+
+message_counter(message)
+
+'''
+
 for character in message.lower():
     count.setdefault(character,0)
     count[character] = count[character] + 1
 
+# print
 print(count)
+'''
